606ac55e89d93598996d53307d4a2dc25215c7c0
benjmhart <benjmhart@gmail.com>
2019-07-15 21:46:41 -0400

    Adjust eslintrc and other settings to reduce lint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,24 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
-    project: './tsconfig.json'
+    project: './tsconfig.json',
+    jsx: true
   },
   plugins: ['@typescript-eslint'],
   env: {
     browser: true,
-    node: true,
+    node: true
   },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended'
   ],
   rules: {
-    "indent": ["error", 2],
-    "no-console": 1
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/indent': ['error', 2],
+    '@typescript-eslint/no-use-before-define': ['error', { functions: false, classes: false, typedefs: false }],
+    'no-console': 1
   }
 };
