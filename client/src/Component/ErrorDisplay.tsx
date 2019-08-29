1ea7eb9ebc1c52da894c53887aa9d2b17b479487
benjmhart <benjmhart@gmail.com>
2019-08-28 20:40:21 -0400

    fix error display

--- client/src/Component/ErrorDisplay.tsx
+++ client/src/Component/ErrorDisplay.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import PropTypes,  { InferProps }  from 'prop-types';
 import Box from 'mineral-ui/Box';
 import Text from 'mineral-ui/Text';
 
-const ErrorDisplay: React.FunctionComponent<{ error: string }> = ({ error }) => {
+const ErrorDisplay = ({ error }
+  : InferProps<typeof ErrorDisplay.propTypes>) => {
   return (
     <Box as="p">
       <Text color="red">{error}</Text>
