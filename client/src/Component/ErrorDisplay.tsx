defe598e45ea835122c66fb522bfd8c163a46556
Neil Rutledge <neilrutledgephoto@gmail.com>
2019-08-28 20:47:28 -0400

    Fix linting issues

--- client/src/Component/ErrorDisplay.tsx
+++ client/src/Component/ErrorDisplay.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import PropTypes,  { InferProps }  from 'prop-types';
+import PropTypes, { InferProps } from 'prop-types';
 import Box from 'mineral-ui/Box';
 import Text from 'mineral-ui/Text';
 
-const ErrorDisplay = ({ error }
-  : InferProps<typeof ErrorDisplay.propTypes>) => {
+const ErrorDisplay = ({ error }: InferProps<typeof ErrorDisplay.propTypes>) => {
   return (
     <Box as="p">
       <Text color="red">{error}</Text>
