24df3d57ecd36fc84d5d5e73eb86619345b70edc
benjmhart <benjmhart@gmail.com>
2019-09-15 15:21:13 -0400

    delete dist files

--- server/dist/serverRun.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = __importDefault(require("./server"));
-const port = parseInt(process.env.PORT || '5000', 10);
-server_1.default(port);
-//# sourceMappingURL=serverRun.js.map
